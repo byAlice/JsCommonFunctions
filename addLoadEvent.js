b3af02a773810ba26a6d39d87d0bc8855172fc1f
byAlice <1533674340@qq.com>
2016-05-29 22:46:09 +0800

    function addLoadEvent（func）

---
+++ addLoadEvent.js
@@ -0,0 +1,18 @@
+/**
+ * Created by lenovo on 2016/5/29.
+ */
+/*“≥√Êº”‘ÿ*/
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != "function"){
+        window.onload = func;
+    }
+    else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+addLoadEvent(firstFunction);
+addLoadEvent(secondFunction);
